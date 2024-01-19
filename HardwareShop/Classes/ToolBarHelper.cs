71daec08da37fe03cfbe22ae9c61b7988f67eb8e
Suchimauzic <sergdog03@gmail.com>
2024-01-19 20:40:35 +0300

    Созданы 2 окна: главное и каталог, есть какой-то интерфейс, есть файлы для работы с toolbar, но это на потом (может вообще исключить)

---
+++ HardwareShop/Classes/ToolBarHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HardwareShop.Classes
+{
+    static internal class ToolBarHelper
+    {
+        static public void close(Window window)
+        {
+            window.Close();
+        }
+    }
+}
---
+++ HardwareShop/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using HardwareShop.Classes;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HardwareShop
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Application.Current.MainWindow.MaxHeight = System.Windows.SystemParameters.PrimaryScreenHeight;
+            Application.Current.MainWindow.MaxWidth = System.Windows.SystemParameters.PrimaryScreenWidth;
+        }
+
+        /*========================================
+                    Работа ToolBar
+        ========================================*/
+
+        private void ToolBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);                // Закрытие программы
+        }
+
+        private void btnMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            // Свёртывание окна
+
+            string path = @"/Resources/";
+
+            if (Application.Current.MainWindow.WindowState == WindowState.Maximized)
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Normal;
+                path += "Maximize_Off_Black.png";
+            }
+            else
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Maximized;
+                path += "Maximize_On_Black.png";
+            }
+
+            //BitmapImage bitmap = new BitmapImage();
+            //bitmap.BeginInit();
+            //bitmap.UriSource = new Uri(path);
+            //bitmap.EndInit();
+
+            //imageMaximize.Source = bitmap;
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void btnWork_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"\"{(sender as Button).Content}\" находиться в разработке, в скором будующем эта функция будет работать!", "Увы...", MessageBoxButton.OK);
+        }
+    }
+}
